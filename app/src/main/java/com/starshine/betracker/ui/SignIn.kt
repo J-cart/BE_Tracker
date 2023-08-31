3767fa93476c9a2bc05772d8f89653a586dce62a
Joseph <j.t.cartoonist@gmail.com>
2023-08-31 19:38:18 +0100

    removed two bottom nav, added dummy auth ...

--- app/src/main/java/com/starshine/betracker/ui/SignIn.kt
+++ app/src/main/java/com/starshine/betracker/ui/SignIn.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.starshine.betracker.R
 import com.starshine.betracker.databinding.FragmentSignInBinding
@@ -22,9 +23,19 @@ class SignIn : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.signInBtn.setOnClickListener {
-            val route = SignInDirections.actionSignInToDashboard()
-            findNavController().navigate(route)
+            val username = binding.userNameEditText.text.toString().trim()
+            val password = binding.userNameEditText.text.toString().trim()
+
+            if (username == "starshine" && password == "123456"){
+                val route = SignInDirections.actionSignInToDashboard()
+                findNavController().navigate(route)
+            }else{
+                Toast.makeText(requireContext(), "Invalid credentials", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
 
     }
